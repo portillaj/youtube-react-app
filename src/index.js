0521a55332c400dfaae56f53ebc1ca8ae95e37cf
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-09-06 17:31:06 -0400

    es6 class components updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAX-BBg9D4Zz5iDgjGuviEIMcsShqVgFmQ';
+
 //Create a new component.  This component should produce
 //some HTML
-const App = () => { return <div>Hi!</div> };
+const App = () => {
+  return (
+    <div>
+    <SearchBar />
+  </div>
+);
+}
 
 //take this component's generate html and put it
 //on thie page (in the DOM)
