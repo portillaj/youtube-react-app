b2c94ecc828cee2894d743a3512f8b2b08112983
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-09-07 11:58:57 -0400

    video list displaying and video is loading and playing :)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAX-BBg9D4Zz5iDgjGuviEIMcsShqVgFmQ';
 
 //Create a new component.  This component should produce
 //some HTML
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'soulchef'}, (videos) => {
+      this.setState({ videos });
+      //same as this.state({videos: videos});
+    });
+
+  }
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      <VideoDetail video={this.state.videos[0]}/>
+      <VideoList  videos={this.state.videos}/>
+    </div>
+  );
+  }
 }
 
 //take this component's generate html and put it
